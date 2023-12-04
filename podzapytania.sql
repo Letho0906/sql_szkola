d2c3df3fb973b77ad0ba34c1306bfc94baedbcf8
Letho0906 <119071816+Letho0906@users.noreply.github.com>
2023-12-04 16:24:29 +0100

    Update podzapytania.sql

--- podzapytania.sql
+++ podzapytania.sql
@@ -2,5 +2,13 @@
 select id 
 from zamowienia
 where wartosc = (select min(wartosc) 
-				 from zamowienia);
+		 from zamowienia);
+
+-- 2 
+select nazwa, id
+from towary
+where id in (select id 
+	     from zamowienia 
+             where wartosc = (select min(wartosc) 
+			      from zamowienia));
           
