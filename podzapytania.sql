7571fc7782418926b8aa809da7bcfb614caa7171
Letho0906 <119071816+Letho0906@users.noreply.github.com>
2023-12-05 16:18:11 +0100

    Update podzapytania.sql

--- podzapytania.sql
+++ podzapytania.sql
@@ -1,10 +1,10 @@
--- 1
+-- 1.1
 select id 
 from zamowienia
 where wartosc = (select min(wartosc) 
 		 from zamowienia);
 
--- 2 
+-- 1.2 
 select nazwa, id
 from towary
 where id in (select id 
@@ -12,7 +12,7 @@ where id in (select id
              where wartosc = (select min(wartosc) 
 			      from zamowienia));
           
---3
+-- 1.3
 select nazwa, grupa, id
 from towary
 where id = (select towar_id 
@@ -20,15 +20,22 @@ where id = (select towar_id
                  where wartosc = (select max(wartosc)
 				  from zamowienia));
 
--- 4
+-- 1.4
 select nazwa, grupa
 from towary
 where cena > (select avg(cena) 
 			  from towary);
 
--- 5 
+-- 2.1
 select count(imie) as "liczba wlascicieli mieszkajacych w bialystoku"
 from wlasciciel
 where wlascicielnr in (select wlascicielnr 
 					  from nieruchomosc 
                       where miasto = "bialystok");
+
+-- 2.2
+select count(biuronr) as "liczba biuro w łomży"
+from biuro
+where biuronr in (select biuronr 
+				  from biuro
+                  where miasto = "lomza");
